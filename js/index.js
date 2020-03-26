b3cada655c6f9a9d9b69871421d7ff97c8dc0ae6
Tony Scaletti <ascaletti@shoplogix.com>
2020-03-26 17:45:34 -0400

    Gutted it

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
-    $("#logoImg").addClass("animated bounce");
-    //Button clicks
-    $("#aboutBtn").click(function(){
-        $('html, body').animate({scrollTop: $("#about").offset().top}, 2000);
-    });
-    $("#aboutBtn").click(function(){
-        $('html, body').animate({scrollTop: $("#about").offset().top}, 1000);
-    });
-    $("#portfolioBtn").click(function(){
-        $('html, body').animate({scrollTop: $("#portfolio").offset().top}, 1000);
-    });
-    $("#linksBtn").click(function(){
-        $('html, body').animate({scrollTop: $("#links").offset().top}, 1000);
-    });
+
 });
