462743847e0494bb35e5a0541b5937f4910d7d83
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-03 15:46:46 -0400

    Completed website MVP

--- js/index.js
+++ js/index.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $("#begin").click(function(){
         $('html, body').animate({scrollTop: $("#bio").offset().top}, 2000);
     });
+
+    $("#martis-hastam-back").click(function(){
+        window.open('https://martis-hastam.herokuapp.com/', '_blank');
+    });
+
+    $("#taskfly-back").click(function(){
+        window.open('https://taskfly-io.herokuapp.com/', '_blank');
+    });
 });
