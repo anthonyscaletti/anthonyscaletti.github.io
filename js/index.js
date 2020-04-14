2016481dee9e5801e66711e0b2c13c9a90fd15bc
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-14 18:11:45 -0400

    Updated project

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $("#bio").offset().top}, 2000);
     });
 
+    $("#covid19-global-tracker-back").click(function(){
+        window.open('https://covid19-global-tracker.herokuapp.com/', '_blank');
+    });
+
     $("#martis-hastam-back").click(function(){
         window.open('https://martis-hastam.herokuapp.com/', '_blank');
     });
